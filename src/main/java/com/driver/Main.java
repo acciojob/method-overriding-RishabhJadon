c65ad51e593fabb332f2bc3cb885673ccf2fbf7b
Rishabh Singh <rishabhjadon976@gmail.com>
2023-09-12 12:09:39 +0530

    second commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,17 +1,6 @@
 package com.driver;
 
-class A
-{
-    public String meth()
-    {
-        return "Method is overridden in Extendend class B";
-    }
-}
 
-class B extends A
-{
-
-}
 public class Main {
 
     A obj = new B();
